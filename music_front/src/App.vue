053adda5855359748e2c975c953255e636e1f997
soroushGH <soroushdeveloper20@gmail.com>
2024-12-24 20:20:53 +0300

    Add vue js , just for learning :)

---
+++ music_front/src/App.vue
@@ -0,0 +1,17 @@
+<script setup>
+</script>
+
+<template>
+  <div class="header"></div>
+</template>
+
+<style scoped>
+.header{
+
+  position: relative;
+  width:100%;
+  height: 60px;
+  background-color: #E9D2F4;
+  margin: 0em;
+}
+</style>
